1779e4cc23b8597391c3869d29c6e0cfbdec929f
Lucas <leblanc.lucas29@gmail.com>
2023-03-06 12:21:34 +0100

    Change order for pretty-print for right-associative extension methods

--- compiler/src/dotty/tools/dotc/printing/RefinedPrinter.scala
+++ compiler/src/dotty/tools/dotc/printing/RefinedPrinter.scala
@@ -920,7 +920,7 @@ class RefinedPrinter(_ctx: Context) extends PlainPrinter(_ctx) {
                 if leftParam.nonEmpty then
                   leftTyParams ::: leadingUsing ::: leftParam ::: trailingUsing ::: rightTyParams ::: rightParam ::: rest6
                 else
-                  tree.paramss // it wasn't a binary operator, after all.
+                  leadingUsing ::: leftTyParams ::: tree.paramss ::: trailingUsing ::: rest6 // it wasn't a binary operator, after all.
               else
                 tree.paramss
             val trailingParamss = paramss
